9f57102e4068c750a41b3cd6910d41b3ad99028f
SerhiiBabych <sergeiy.babich@gmail.com>
2025-09-15 20:33:34 +0200

    total update

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,4 @@
- // Переменная для хранения текущего языка в памяти
+
         let currentLang = 'de';
         
         const translations = {
@@ -7,19 +7,17 @@
         };
         
         function setLang(lang) {
-            // Сохраняем выбор в переменной
+
             currentLang = lang;
-            
-            // Меняем текст
+
             document.getElementById("lang-text").textContent = translations[lang];
-            
-            // Сбрасываем стили всех кнопок (неактивные)
+
             document.getElementById("btn-en").className = 
                 "px-3 py-1 rounded border border-blue-500  text-blue-500 hover:bg-blue-50 transition-colors";
             document.getElementById("btn-de").className = 
                 "px-3 py-1 rounded border border-blue-500  text-blue-500 hover:bg-blue-50 transition-colors";
             
-            // Подсвечиваем активную кнопку
+
             if (lang === "en") {
                 document.getElementById("btn-en").className = 
                     "px-3 py-1 rounded border border-blue-500 bg-blue-500 text-white shadow-md";
@@ -29,6 +27,5 @@
             }
         }
         
-        // Инициализация с немецким языком по умолчанию
         setLang('de');
 
